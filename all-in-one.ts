cef77345a4bb8613d1b2d3527201a8cb5fc99099
Mike Cann <mike.cann@gmail.com>
2019-06-24 11:25:50 +0800

    added pick

--- all-in-one.ts
+++ all-in-one.ts
@@ -118,7 +118,6 @@
 }
 
 {
-
   type User = {
     name: string;
     age: number;
@@ -144,7 +143,7 @@
   addUserToDB(user2); // hmm.. we dont get an error?
 
   // This is because typescript uses "Structural Subtyping", so if it looks like a duck then it is a duck
-  
+
   // This was wierd to be at first coming from C# with its "Nominal Subtypes" but I now love its increased
   // flexibility
 
@@ -152,11 +151,147 @@
 
   // Well one way to do it is like this:
 
-  const user3: User =  {
+  const user3: User = {
     name: "jane",
     age: 100,
     foo: "bar" // now we get an error
-  }
+  };
 
   // But what if you want to strictly define it on the function definition?
+
+  // Turns out this is tricker that you would have thought. Before we get there we need to back
+  // up a little and talk about "keyof"
+}
+
+{
+  // Suppose we want to write a function that "picks" a subset of properties from a provided
+  // object and returns them?
+
+  console.log(pick({ name: "mike", age: 34 }, ["name"])); // { name: "mike" }
+
+  function pick(obj: any, keysToPick: any): any {
+    let newObj = {};
+    for (let key in obj) {
+      if (keysToPick.includes(key)) {
+        newObj[key] = obj[key];
+      }
+    }
+    return newObj;
+  }
+
+  // Cool, very handy, but how to we make this strongly typed?
+
+  // Well we can start by saying that we expect the keys to be an array of strings..
+
+  function pick2(obj: any, keysToPick: string[]): any {
+    // ...
+  }
+
+  // Okay now what?
+
+  // Well typescript has a powerful generics system what we might beable to leverage
+
+  // Lets look at a quick example:
+
+  // We can write a function that takes in a generic and returns another generic like this:
+
+  function ensureThingIsNotNull<T>(theThing: T | null): T {
+    if (theThing == null) throw new Error("nope");
+    return theThing;
+  }
+
+  ensureThingIsNotNull(null);
+  ensureThingIsNotNull("foo");
+  ensureThingIsNotNull(1);
+
+  // So now back to our pick
+
+  function pick3<T>(obj: T, keysToPick: string[]): any {
+    // T but only with the keys from "keysToPick"
+    // ...
+  }
+
+  // Well there is a handy keyword called "keyof"
+
+  function pick4<T>(obj: T, keysToPick: (keyof T)[]): any {
+    // ...
+  }
+
+  // Interesting so now the keys MUST belong to T
+
+  pick4({ name: "mike", age: 34 }, ["name", "age"]); // awesome it allows what it should
+  pick4({ name: "mike", age: 34 }, ["foo"]); // and doesnt allow what it shouldnt
+
+  // But the return types is "any" still.. thats no good
+
+  {
+    const picked = pick4({ name: "mike", age: 34 }, ["name"]); // awesome it allows what it should
+  }
+
+  // So the final step is to say that the return type is going to be an object that includes
+  // the keys we supplied
+
+  function pick5<T>(obj: T, keysToPick: (keyof T)[]): { [P in keyof T]: any } {
+    return {} as any;
+  }
+
+  {
+    const picked = pick5({ name: "mike", age: 34 }, ["name"]); // hmm it doesnt?
+  }
+
+  // The problem is we arent specifying that the keys we are supplying are going to be the
+  // only keys on the new object, to do that we have to make it a generic argument too:
+
+  function pick6<T, U>(obj: T, keysToPick: U[]): { [P in U]: any } {
+    return {} as any;
+  }
+
+  // Hmm.. But how do we specify that U should be the keyof T ?
+
+  // Type constraints to the rescue!
+
+  function pick7<T, U extends keyof T>(
+    obj: T,
+    keysToPick: U[]
+  ): { [P in U]: any } {
+    return {} as any;
+  }
+
+  {
+    const picked = pick7({ name: "mike", age: 34 }, ["name"]); // yey! it now works
+  }
+
+  // One thing tho is we have lost the type of the properties on the returned object
+  // but we can get them back again by looking up the property on U
+
+  function pick8<T, U extends keyof T>(
+    obj: T,
+    keysToPick: U[]
+  ): { [P in U]: T[P] } {
+    return {} as any;
+  }
+
+  {
+    const picked = pick8({ name: "mike", age: 34 }, ["name", "age"]); // yey! it now works
+  }
+
+  // Awesome! But one issue:
+
+  pick8("foo", ["bar"]);
+  pick8(1, ["bar"]);
+
+  // We can call the pick with any old type.. Thoughts on how to solve this?
+
+  function pick9<T extends object, U extends keyof T>(
+    obj: T,
+    keysToPick: U[]
+  ): { [P in U]: T[P] } {
+    return {} as any;
+  }
+
+  {
+    const picked = pick9({ name: "mike", age: 34 }, ["name", "age"]); // yey! it now works
+    pick9("foo", ["bar"]);
+    pick9(1, ["bar"]);
+  }
 }
