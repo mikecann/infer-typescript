7cbd4b60e0b582bf66f2e94231c1b6ed600af225
Mike Cann <mike.cann@gmail.com>
2019-08-01 15:32:37 +0800

    rejigged again

---
+++ live/bonus.ts
@@ -0,0 +1,19 @@
+type InferTest<T> =
+  T extends (infer U)[] ? U :
+  T extends (...args: any[]) => infer U ? U :
+  T extends Promise<infer U> ? U :
+  T;
+
+type T0 = InferTest<boolean>;
+type T1 = InferTest<string[]>;
+type T2 = InferTest<() => number>;
+type T3 = InferTest<Promise<number>>;
+type T4 = InferTest<Promise<string>[]>;
+type T5 = InferTest<InferTest<Promise<boolean>[]>>;
+
+// These?
+
+type MoreInferTest<T> = T extends { a: infer U; b: infer U } ? U : never;
+
+type T6 = MoreInferTest<{ a: string; b: string }>;
+type T7 = MoreInferTest<{ a: string; b: number }>; 
