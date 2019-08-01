d4969524f9c1186cfe896e0a518537a810d76203
Mike Cann <mike.cann@gmail.com>
2019-08-01 16:21:48 +0800

    minor tweak to bonus

--- live/bonus.ts
+++ live/bonus.ts
@@ -8,7 +8,7 @@ type T0 = InferTest<boolean>;
 type T1 = InferTest<string[]>;
 type T2 = InferTest<(a: string) => number>;
 type T3 = InferTest<Promise<number>>;
-type T4 = InferTest<Promise<string>[]>;
+type T4 = InferTest<Promise<string>>[];
 type T5 = InferTest<InferTest<Promise<boolean>[]>>;
 
 // These?
